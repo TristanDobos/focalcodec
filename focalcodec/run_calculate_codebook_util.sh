c5bbb65885e01c128ad42b232e3117dd15dea713
Tristan Dobos <tristandobos37@gmail.com>
2026-05-04 23:59:51 +0200

    add calculate codebook utilization

---
+++ focalcodec/run_calculate_codebook_util.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#$ -o /mnt/matylda6/xdobos00/runs/logs/codebook-util-$JOB_NAME.$JOB_ID.out            # standard output log file
+#$ -e /mnt/matylda6/xdobos00/runs/logs/codebook-util-$JOB_NAME.$JOB_ID.err            # standard error log file
+
+
+
+if [ -z "$ALLOWED_GPUS" ]; then
+    echo "Error: ALLOWED_GPUS environment variable is not set."
+    exit 1
+fi
+export CUDA_VISIBLE_DEVICES=$(~/scripts/free-gpus.sh "$ALLOWED_GPUS")
+
+if [[ "$EXP_NAME" == na* ]]; then
+    source /mnt/matylda6/xdobos00/nemo_final/bin/activate
+    python reconstruct_nemo_cuda.py "$EXP_NAME"
+else
+    source /mnt/matylda6/xdobos00/miniconda/etc/profile.d/conda.sh
+    conda activate focal-pv310
+fi
+
